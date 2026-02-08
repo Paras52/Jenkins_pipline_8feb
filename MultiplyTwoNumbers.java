71309da742dda22c19662e4f1dfd134436715cd8
Paras52 <114329122+Paras52@users.noreply.github.com>
2026-02-08 11:31:44 +0530

    Change second number and duplicate output statements
    Updated the second number for multiplication and added duplicate print statements.

--- MultiplyTwoNumbers.java
+++ MultiplyTwoNumbers.java
@@ -3,10 +3,14 @@ public class MultiplyTwoNumbers {
     public static void main(String[] args) {
 
         float first = 1.6f;
-        float second = 2.0f;
+        float second = 2.1f;
 
         float product = first * second;
 
         System.out.println("The product is: " + product);
+                System.out.println("The product is: " + product);
+                        System.out.println("The product is: " + product);
+                        System.out.println("The product is: " + product);
+
     }
 }
